800724858ee37bf5a6e39e9f2569462e5223cec4
Dusan Mijatovic (Linux) <d.mijatovic@dv4all.nl>
2018-04-15 15:30:26 +0200

    run only CounterComponent test

--- src/app/shared/counter/counter.component.spec.ts
+++ src/app/shared/counter/counter.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CounterComponent } from './counter.component';
 
-describe('CounterComponent', () => {
+fdescribe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
 
